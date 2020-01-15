3f0db7017c0802211098d3b568c4149971bab5a2
League Student <league@iMac-27.attlocal.net>
2020-01-14 19:58:32 -0800

    sortArrayChecker ip

--- src/_00_Sorting_Algorithms/_00_SortedArrayChecker.java
+++ src/_00_Sorting_Algorithms/_00_SortedArrayChecker.java
@@ -1,19 +1,37 @@
 package _00_Sorting_Algorithms;
 
 public class _00_SortedArrayChecker {
+	static int p;
+	static double k;
 	//1. Write a static method called intArraySorted. 
 	//   This method takes in an array of integers
 	//   and it returns a boolean.
 	//   The method returns true if the integer
 	//   array is in ascending order and false otherwise
 
-	
+	static boolean intArraySorted(int[] intArray) {
+		for (int i = 0; i < intArray.length-1; i++) {
+		p=intArray[i];
+		if(intArray[i+1]<p) {
+			return false;
+		}
+		}
+		return true;
+	}
 	//2. Write a static method called doubleArraySorted. 
 	//   This method takes in an array of doubles
 	//   and it returns a boolean.
 	//   The method returns true if the double
 	//   array is in ascending order and false otherwise
-
+	static boolean doubleArraySorted(double[] doubleArray) {
+		for (int i = 0; i < doubleArray.length-1; i++) {
+			k=doubleArray[i];
+			if(doubleArray[i+1]<k) {
+				return false;
+			}
+			}
+			return true;
+	}
 	
 	//3. Write a static method called charArraySorted. 
 	//   This method takes in an array of characters
@@ -21,7 +39,13 @@ public class _00_SortedArrayChecker {
 	//   The method returns true if the character
 	//   array is in alphabetical order and false otherwise
 	//   (You can compare characters just like integers)
-
+	
+	/*
+	 * static boolean charArraySorted(char[] charArray) { for (int i = 0; i <
+	 * charArray.length-1; i++) { k=charArray[i]; if(charArray[i+1]<k) { return
+	 * false; } } return true; }
+	 */
+	
 	
 	//4. Write a static method called stringArraySorted. 
 	//   This method takes in an array of Strings
